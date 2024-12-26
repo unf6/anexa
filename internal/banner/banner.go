3242db5cfe38f5d940357e06202d781114269eb0
Xretic <95174481+unf6@users.noreply.github.com>
2024-12-26 18:21:05 +0300

    Update banner.go

--- internal/banner/banner.go
+++ internal/banner/banner.go
@@ -1,14 +1,13 @@
 package banner
 
 import (
-	
 	"os"
 	"os/exec"
 	"runtime"
 )
 
 func clear_screen() {
-	var clearCmd * exec.Cmd
+	var clearCmd *exec.Cmd
 	if runtime.GOOS == "windows" {
 		clearCmd = exec.Command("cmd", "/c", "cls")
 	} else {
